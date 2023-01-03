be07ad0445bc9751595d4b6cae34e909c2a0bbaf
Elliot Pettingale <elliot@smartercoding.co.uk>
2023-01-03 09:15:49 +0000

    add films table

---
+++ database/migrations/2023_01_03_085337_create_films_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('title');
+            $table->date('release');
+            $table->longText('summary');
+            $table->string('image');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+};
