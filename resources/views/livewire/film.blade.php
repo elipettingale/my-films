cb310970e13ebbee704d5506aad1ef12aedef26d
Elliot Pettingale <elliot@smartercoding.co.uk>
2023-01-03 09:31:02 +0000

    add simple hover effects

--- resources/views/livewire/film.blade.php
+++ resources/views/livewire/film.blade.php
@@ -1,6 +1,8 @@
 <div class="w-full">
-    <div class="bg-white rounded-lg overflow-hidden shadow relative">
-        <img class="h-56 w-full object-cover object-center" src="{{ $film->image }}" alt="{{ $film->title }}">
+    <div class="bg-white rounded-lg overflow-hidden shadow relative cursor-pointer">
+        <div class="overflow-hidden">
+            <img class="h-96 w-full object-cover object-center hover:scale-110 transition ease-in-out" src="{{ $film->image }}" alt="{{ $film->title }}">
+        </div>
         <div class="p-4 h-auto md:h-40 lg:h-48">
             <p class="block font-semibold mb-2 text-lg md:text-base lg:text-lg">
                 {{ $film->title }} ({{ $film->release->format('Y') }})
